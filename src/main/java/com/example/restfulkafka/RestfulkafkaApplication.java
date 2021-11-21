19471b6794268a657787f631623a24a5d30ad040
maktsakalis <maktsakalis@gmail.com>
2021-11-21 13:28:56 +0200

    Initial commit

---
+++ src/main/java/com/example/restfulkafka/RestfulkafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulkafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfulkafka/RestfulkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
